92b5d5648a5056079ffffd68c137510670325dbd
Richard Palmer <ricky@rpgrafx.com>
2019-06-22 14:12:55 -0400

    Partially Functional Rebinding(P1 only)

--- Infinite Duel/Assets/Scripts/UI/BindingUI.cs
+++ Infinite Duel/Assets/Scripts/UI/BindingUI.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using Duel.Input;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Duel.UI
 {
-    public class BindingUI : MonoBehaviour
+    public class BindingUI : MonoBehaviour, ISubmitHandler
     {
         [Rewired.ActionIdProperty(typeof(Duel.Input.Constants.Action))]
         public int actionID;
@@ -15,6 +16,8 @@ namespace Duel.UI
         [SerializeField]
         private TMPro.TextMeshProUGUI bindingText;
 
+        private int elementMapId;
+
         private ControlConfigManager manager;
 
         public void Initialize(ControlConfigManager manager)
@@ -22,9 +25,14 @@ namespace Duel.UI
             this.manager = manager;
         }
 
+        public void OnSubmit(BaseEventData eventData)
+        {
+            manager.StartRebinding(actionID, axisRange, elementMapId);
+        }
+
         public void Refresh()
         {
-            bindingText.text = manager.GetControllerElementNameForAction(actionID);
+            bindingText.text = manager.GetControllerElementForAction(actionID, axisRange, out elementMapId);
         }
     }
 }
--- Infinite Duel/Assets/Scripts/UI/ControlConfigManager.cs
+++ Infinite Duel/Assets/Scripts/UI/ControlConfigManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
 using System;
+using Sirenix.OdinInspector;
 
-namespace Duel.Input
+namespace Duel.UI
 {
-    public class ControlConfigManager : MonoBehaviour
+    public class ControlConfigManager : SerializedMonoBehaviour
     {
-        [Rewired.PlayerIdProperty(typeof(Constants.Player))]
+        [SerializeField]
+        [LabelText("Binding UIs")]
+        private BindingUI[] actionBindingUIs;
+
+        [Rewired.PlayerIdProperty(typeof(Input.Constants.Player))]
         public int currentPlayerId;
 
         public ControllerType selectedControllerType;
@@ -23,7 +28,7 @@ namespace Duel.Input
 
         public ControllerMap CurrentControllerMap
         {
-            get => CurrentPlayer.controllers.maps.GetMap(CurrentController.type, CurrentController.id, Constants.Category.Default, Constants.Layout.Keyboard.Default);
+            get => CurrentPlayer.controllers.maps.GetMap(CurrentController.type, CurrentController.id, Input.Constants.Category.Default, Input.Constants.Layout.Keyboard.Default);
         }
 
         private Controller CurrentController
@@ -31,16 +36,32 @@ namespace Duel.Input
             get => CurrentPlayer.controllers.GetController(selectedControllerType, selectedControllerId);
         }
 
-        public string GetControllerElementNameForAction(int actionId)
+        public string GetControllerElementForAction(int actionId, AxisRange range, out int elementMapId)
         {
-            return CurrentPlayer.controllers.maps.GetFirstElementMapWithAction(actionId, true).elementIdentifierName;
+            //There is an element map for each part of an action, so Axis actions have two, As Such each must be iterated through
+            foreach (var elementMap in CurrentPlayer.controllers.maps.ElementMapsWithAction(actionId, true))
+            {
+                //returns the first element which matches the axis range
+                if (elementMap.ShowInField(range))
+                {
+                    elementMapId = elementMap.id;
+
+                    return elementMap.elementIdentifierName;
+                }
+            }
+            elementMapId = -1;
+            return string.Empty;
         }
 
         private void OnEnable()
         {
+            foreach (BindingUI ui in actionBindingUIs)
+            {
+                ui.Initialize(this);
+            }
+
             if (!ReInput.isReady)
                 return;
-            Debug.Log(CurrentPlayer.controllers.maps.GetFirstElementMapWithAction(Input.Constants.Action.Jump, true).elementIdentifierName);
 
             inputMapper.options.ignoreMouseXAxis = true;
             inputMapper.options.ignoreMouseYAxis = true;
@@ -49,6 +70,8 @@ namespace Duel.Input
             ReInput.ControllerDisconnectedEvent += OnControllerChanged;
             inputMapper.InputMappedEvent += OnInputMapped;
             inputMapper.StoppedEvent += OnStopped;
+
+            Refresh();
         }
 
         private void OnDisable()
@@ -61,15 +84,40 @@ namespace Duel.Input
 
         private void OnStopped(InputMapper.StoppedEventData obj)
         {
+            CurrentPlayer.controllers.maps.SetMapsEnabled(true, Input.Constants.Category.UI);
         }
 
         private void OnInputMapped(InputMapper.InputMappedEventData obj)
         {
+            Refresh();
         }
 
         private void OnControllerChanged(ControllerStatusChangedEventArgs obj)
         {
             print(obj.name);
+        }
+
+        public void StartRebinding(int actionID, AxisRange axisRange, int elementMapId)
+        {
+            CurrentPlayer.controllers.maps.SetMapsEnabled(false, Input.Constants.Category.UI);
+
+            inputMapper.Start(
+               new InputMapper.Context()
+               {
+                   actionId = actionID,
+                   controllerMap = CurrentControllerMap,
+                   actionRange = axisRange,
+                   actionElementMapToReplace = CurrentControllerMap.GetElementMap(elementMapId)
+               }
+           );
+        }
+
+        private void Refresh()
+        {
+            foreach (BindingUI ui in actionBindingUIs)
+            {
+                ui.Refresh();
+            }
         }
     }
 }
